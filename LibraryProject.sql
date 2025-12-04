0ac9b1b5f72b1848e93094ba4a9e0c0d9e4c3938
D <ljon188@lsu.edu>
2025-12-03 20:25:46 -0600

    added sample data
    added sample data to main project file

--- LibraryProject.sql
+++ LibraryProject.sql
@@ -198,3 +198,303 @@ SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2025-12-03 14:20:19
+
+
+
+-- added sample data
+
+
+-- Books
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (1338878921, 'Fiction', 'J.K. Rowling', 'Harry Potter and the Sorcerer’s Stone', 'Scholastic', 'teens');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0544273443, 'Fantasy', 'J.R.R. Tolkien', 'Lord of the Rings Deluxe Edition', 'Allen & Unwin', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0573702411, 'Mystery', 'Agatha Christie', 'Death on the Nile', 'Collins Crime Club', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (1455582875, 'Romance', 'Nicholas Sparks', 'The Notebook', 'Warner Books', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0553382578, 'Sci-Fi', 'Isaac Asimov', 'Foundation', 'Gnome Press', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (1501143107, 'Thriller', 'Stephen King', 'Misery', 'Viking', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0553386794, 'Fantasy', 'George R.R. Martin', 'A Game of Thrones', 'Bantam Spectra', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0143039989, 'Horror', 'Shirley Jackson', 'The Haunting of Hill House', 'Viking', 'teens');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0743270754, 'History', 'Doris Kearns Goodwin', 'Team of Rivals: The Political Genius of Abraham Lincoln', 'Simon & Schuster', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0316017930, 'Non-fiction', 'Malcolm Gladwell', 'Outliers: The Story of Success', 'Little, Brown and Company', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0241265541, 'Fiction', 'Leo Tolstoy', 'War and Peace', 'The Russian Messenger', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (1982176865, 'Biography', 'Walter Isaacson', 'Steve Jobs', 'Simon & Schuster ', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0060244887, 'Fantasy', 'C.S. Lewis', 'The Chronicles of Narnia Box Set', 'HarperCollins', 'kids');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0143107321, 'Adventure', 'Mark Twain', 'The Adventures of Huckleberry Finn', 'Chatto & Windus', 'teens');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0553897446, 'Mystery', 'Arthur Conan Doyle', 'The Adventures of Sherlock Holmes', 'George Newnes', 'teens');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0141439513, 'Romance', 'Jane Austen', 'Pride and Prejudice', 'T. Egerton', 'teens');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0140569324, 'Picture Book', 'Eric Carle', 'The Very Hungry Caterpillar', 'World Publishing Company', 'kids');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (1558585362, 'Picture Book', 'Marcus Pfister', 'The Rainbow Fish', 'NordSüd Verlag', 'kids');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (0385504217, 'Thriller', 'Dan Brown', 'The Da Vinci Code', 'Doubleday', 'adults');
+
+INSERT INTO book (ISBN, genre, author, title, publisher, department)
+VALUES (1954839243, 'Fiction', 'F. Scott Fitzgerald', 'The Great Gatsby', 'Charles Scribner’s Sons', 'teens');
+
+
+
+-- CDs
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (546737758, 'Synth-pop', '1989', 'Taylor Swift', 'Big Machine Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (621371203, 'Pop', 'Divide', 'Ed Sheeran', 'Atlantic Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (761901941, 'Pop', '21', 'Adele', 'XL Recordings', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (992369051, 'Rock', 'Abbey Road', 'The Beatles', 'Apple Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (757860280, 'Hip-Hop', 'Views', 'Drake', 'Cash Money Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (674982918, 'R&B', 'Lemonade', 'Beyonce', 'Columbia Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (438298552, 'Pop', 'Teenage Dream', 'Katy Perry', 'Capitol Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (940080422, 'Pop', 'Starboy', 'The Weeknd', 'XO Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (235762481, 'Psychedelic Rock', 'Dark Side of the Moon', 'Pink Floyd', 'Capitol Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (824538978, 'Pop', 'Thriller', 'Michael Jackson', 'Epic Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (348638078,'EDM', 'Purpose', 'Justin Bieber', 'Def Jam Recordings', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (669080241, 'Soul', 'In the Lonely Hour', 'Sam Smith', 'Capitol Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (264390199, 'Synth-pop', 'The Fame', 'Lady Gaga', 'Interscope Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (635100828, 'Rock and Roll', 'Born to Run', 'Bruce Springsteen', 'Columbia Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (604061697, 'Pop Rock', 'Songs About Jane', 'Maroon 5', 'Octone Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (586158137, 'Grunge', 'Nevermind', 'Nirvana', 'DGC Records', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (666857876, 'Pop', 'Anti', 'Rihanna', 'Westbury Road', 'adults');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (753114630, 'Alt Pop', 'Born to Die', 'Lana Del Rey', 'Interscope Records', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (699016854, 'Indie Rock', 'AM', 'Arctic Monkeys', 'Domino Recording', 'teens');
+
+INSERT INTO cd (ISSN, genre, title, artist, record_label)
+VALUES (276484285, 'Indie Pop', 'Lungs', 'Florence + The Machine', 'Universal Music', 'teens');
+
+
+
+-- Members
+INSERT INTO member (member_id, name, dob)
+VALUES (5001, 'Jill Stoney', 1992-11-14);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5002, 'Robert Robertson', 1990-01-13);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5003, 'Mia Jones', 1983-08-14);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5004, 'Tom Smith', 1987-10-18);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5005, 'Emma Thompson', 1990-04-15);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5006, 'Tom Sutherland', 1996-06-01);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5007, 'Abbie Coleman', 1996-09-01);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5008, 'John Doe', 1963-06-09);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5009, 'Scarlett Black', 1984-11-22);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5010, 'Chris Townsend', 1983-08-11);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5011, 'Natalie Portman', 1981-06-09);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5012, 'Sam Freeman', 1967-11-22);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5013, 'Chris White', 1981-06-13);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5014, 'Jackie Johnson', 1965-04-04);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5015, 'Sammy Jackson', 1948-12-21);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5016, 'Brie Stacy', 1989-10-01);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5017, 'Jameson Parker', 1976-07-19);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5018, 'May Fontenot', 1985-04-30);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5019, 'Hugh Jones', 1968-10-12);
+
+INSERT INTO member (member_id, name, dob)
+VALUES (5020, 'Jack Red', 1971-01-07);
+
+
+
+
+-- Employees
+INSERT INTO employee (employee_id, email, name)
+VALUES (6001, 'ajohnson12@library.gov', "Alice Johnson");
+
+INSERT INTO employee (employee_id, email, name)
+VALUES (6002, 'bsmith89@library.gov', "Bob Smith");
+
+INSERT INTO employee (employee_id, email, name)
+VALUES (6003, 'cwhite74@library.gov', "Carol White");
+
+INSERT INTO employee (employee_id, email, name)
+VALUES (6004, 'dthomas56@library.gov', "David Thomas");
+
+INSERT INTO employee (employee_id, email, name)
+VALUES (6005, 'miller22@library.gov', "Emily Miller");
+
+
+-- inventory, books/cds can have muliple copies
+
+-- books
+-- Harry Potter and the Sorcerer’s Stone, 3 copies
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7001, '1338878921', NULL);
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7002, '1338878921', NULL);
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7003, '1338878921', NULL);
+
+-- Lord of the Rings Deluxe Edition, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7004, '0544273443', NULL);
+
+-- Death on the Nile, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7005, '0573702411', NULL);
+
+-- Misery, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7006, '1501143107', NULL);
+
+-- The Notebook, 2 copies
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7007, '1455582875', NULL);
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7008, '1455582875', NULL);
+
+-- A Game of Thrones, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7009, '0553386794', NULL);
+
+-- Foundation, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7010, '0553382578', NULL);
+
+-- The Great Gatsby, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7011, '1954839243', NULL);
+
+-- The Haunting of Hill House, 2 copies
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7012, '0143039989', NULL);
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7013, '0143039989', NULL);
+
+
+-- cds
+-- 1989 - Taylor Swift, 2 copies
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7014, NULL, '546737758');
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7015, NULL, '546737758');
+
+-- Divide - Ed Sheeran, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7016, NULL, '621371203');
+
+-- Abbey Road - The Beatles, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7017, NULL, '992369051');
+
+-- Dark Side of the Moon - Pink Floyd, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7018, NULL, '235762481');
+
+-- 21, 2 copies
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7019, NULL, '761901941');
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7020, NULL, '761901941');
+
+-- Teenage Dream, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7021, NULL, '438298552');
+
+-- Starboy, 1 copy
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7022, NULL, '940080422');
+
+-- Thriller, 3 copies
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7023, NULL, '824538978');
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7024, NULL, '824538978');
+INSERT INTO inventory (inventory_id, book_id, cd_id) VALUES (7025, NULL, '824538978');
+
+
+-- loans
+-- --- Active Loans (return_date IS NULL) ---
+-- Jill Stoney (5001) borrows Harry Potter Copy 1 (7001)
+INSERT INTO loan (loan_id, inventory_id, borrow_date, due_date, member_id, return_date)
+VALUES (8001, 7001, '2025-11-20', '2025-12-11', 5001, NULL);
+
+-- Robert Robertson (5002) borrows The Notebook Copy 1 (7005)
+INSERT INTO loan (loan_id, inventory_id, borrow_date, due_date, member_id, return_date)
+VALUES (8002, 7005, '2025-11-25', '2025-12-16', 5002, NULL);
+
+-- Mia Jones (5003) borrows 1989 CD Copy 2 (7010)
+INSERT INTO loan (loan_id, inventory_id, borrow_date, due_date, member_id, return_date)
+VALUES (8003, 7010, '2025-11-28', '2025-12-19', 5003, NULL);
+
+-- --- Completed Loans (return_date IS NOT NULL) ---
+-- Tom Smith (5004) borrowed and returned Lord of the Rings (7003)
+INSERT INTO loan (loan_id, inventory_id, borrow_date, due_date, member_id, return_date)
+VALUES (8004, 7003, '2025-10-01', '2025-10-22', 5004, '2025-10-18');
+
+-- Emma Thompson (5005) borrowed and returned Abbey Road CD (7012)
+INSERT INTO loan (loan_id, inventory_id, borrow_date, due_date, member_id, return_date)
+VALUES (8005, 7012, '2025-10-15', '2025-11-05', 5005, '2025-11-05');
